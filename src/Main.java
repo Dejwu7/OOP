6ba84ff8c9e9bb3afbaeaae73a4244a0e38220a2
Dejwu7 <dawid.misiak204@gmail.com>
2025-03-07 14:05:55 +0100

    lab1

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         point.y = 12.0;
         System.out.println(point.x + " \n" + point.y);
         System.out.println(point.to_String());
+        System.out.println(point.toSvg());
     }
 }
--- src/Point.java
+++ src/Point.java
@@ -4,4 +4,10 @@ public class Point {
     {
         return "x= "+x+" y= "+y;
     }
+    public String toSvg(){
+       return  "<svg height=\"100\" width=\"100\" xmlns=\"http://www.w3.org/2000/svg\">"+
+               "<circle r=\"45\" cx="+x+"cy="+y+"fill=\"red\" />"+
+               "</svg>";
+
+    }
 }
