a20b13ccd5fde83e74f0768cc35075423a74842b
Dejwu7 <dawid.misiak204@gmail.com>
2025-03-07 13:56:10 +0100

    Add files via upload

---
+++ src/Zadanie1LAB1/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point();
+        point.x = 10.0;
+        point.y = 12.0;
+        System.out.println(point.x + " \n" + point.y);
+        System.out.println(point.to_String());
+    }
+}
